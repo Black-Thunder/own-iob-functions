3fe836b659bd653d8397d376be0f87b42570701f
Black-Thunder <972581+Black-Thunder@users.noreply.github.com>
2024-07-25 15:36:29 +0200

    moved all modules to one single module

---
+++ index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const airConditionerControlFunctions = require("./lib/airConditionerControlFunctions.js");
+const airPurifierControlFunctions = require("./lib/airPurifierControlFunctions.js");
+const alexaControlFunctions = require("./lib/alexaControlFunctions.js");
+const commonDefines = require("./lib/commonDefines.js");
+const commonFunctions = require("./lib/commonFunctions.js");
+const customLoggingFunctions = require("./lib/customLoggingFunctions.js");
+const dateTimeFunctions = require("./lib/dateTimeFunctions.js");
+const lightingControlFunctions = require("./lib/lightingControlFunctions.js");
+const pingFunctions = require("./lib/pingFunctions.js");
+const presenceFunctions = require("./lib/presenceFunctions.js");
+const pushoverFunctions = require("./lib/pushoverFunctions.js");
+const sensorFunctions = require("./lib/sensorFunctions.js");
+const smartPlugControlFunctions = require("./lib/smartPlugControlFunctions.js");
+const sunblindControlFunctions = require("./lib/sunblindControlFunctions.js");
+const telegramFunctions = require("./lib/telegramFunctions.js");
+const thermostatControlFunctions = require("./lib/thermostatControlFunctions.js");
+const vacuumCleanerFunctions = require("./lib/vacuumCleanerFunctions.js");
+const weatherSensorFunctions = require("./lib/weatherSensorFunctions.js");
+
+module.exports = () => {
+    return {
+        airConditionerControlFunctions,
+        airPurifierControlFunctions,
+        alexaControlFunctions,
+        customLoggingFunctions,
+        dateTimeFunctions,
+        lightingControlFunctions,
+        pingFunctions,
+        presenceFunctions,
+        pushoverFunctions,
+        sensorFunctions,
+        smartPlugControlFunctions,
+        sunblindControlFunctions,
+        telegramFunctions,
+        thermostatControlFunctions,
+        vacuumCleanerFunctions,
+        weatherSensorFunctions,
+        commonDefines,
+        commonFunctions
+    }
+}
